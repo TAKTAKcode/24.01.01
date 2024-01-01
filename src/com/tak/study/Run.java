5441d49b3630e8b140c1ef38400f7dda45eb4e3f
tak <wontaek22@gmail.com>
2024-01-01 17:30:33 +0900

    24.01.01
    프로그래머스 (달리기 경주)

---
+++ src/com/tak/study/Run.java
@@ -0,0 +1,26 @@
+package com.tak.study;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Run {
+    public String[] solution(String[] players, String[] callings) {
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < players.length; i++) {
+            map.put(players[i], i);
+        }
+
+        for (String s : callings) {
+            int cur = map.get(s);
+            map.put(s, cur - 1);
+            map.put(players[cur - 1], cur);
+            players[cur] = players[cur - 1];
+            players[cur - 1] = s;
+        }
+
+        return players;
+
+    }
+}
